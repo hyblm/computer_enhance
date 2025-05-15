6f2afa471c2b9a177ecd72f0414ddb146405f705
hyblm <hyma.code@gmail.com>
2025-05-15 14:46:50 +0200

    fixes

--- part2/src/bin/generate_json.rs
+++ part2/src/bin/generate_json.rs
@@ -1,3 +1,5 @@
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+
 use std::{
     env::args,
     fs::File,
@@ -9,17 +11,17 @@ use std::{
 use rand::{Rng, SeedableRng};
 use rand_chacha::ChaCha20Rng;
 
-const Y_EXTENT: f64 = 180.0;
-const X_EXTENT: f64 = 90.0;
-const Y_RANGE: RangeInclusive<f64> = -Y_EXTENT..=Y_EXTENT;
+const X_EXTENT: f64 = 180.0;
+const Y_EXTENT: f64 = 90.0;
 const X_RANGE: RangeInclusive<f64> = -X_EXTENT..=X_EXTENT;
+const Y_RANGE: RangeInclusive<f64> = -Y_EXTENT..=Y_EXTENT;
 
 const CLUSTER_SIZE: f64 = 10.;
-const Y_CLUSTER_RANGE: RangeInclusive<f64> = -Y_EXTENT..=Y_EXTENT - CLUSTER_SIZE;
 const X_CLUSTER_RANGE: RangeInclusive<f64> = -X_EXTENT..=X_EXTENT - CLUSTER_SIZE;
+const Y_CLUSTER_RANGE: RangeInclusive<f64> = -Y_EXTENT..=Y_EXTENT - CLUSTER_SIZE;
 
 fn main() {
-    let options = process_args();
+    let options = process_args_or_bail();
     options.print();
 
     let Options {
@@ -41,24 +43,21 @@ fn main() {
             haversine::reference_haversine(x0, y0, x1, y1, radius)
         })
         .collect();
+
     let sum: f64 = distances.iter().sum();
     println!("Expected sum: {sum}");
 
-    let distances: Vec<u8> = distances
-        .into_iter()
-        .map(f64::to_le_bytes)
-        .flatten()
-        .collect();
+    let distances_bin: Vec<u8> = distances.into_iter().flat_map(f64::to_le_bytes).collect();
 
     let answers_file = File::create("haversine_distance_answers.bin").unwrap();
     let mut answers_w = BufWriter::new(answers_file);
-    answers_w.write_all(&distances).unwrap();
+    answers_w.write_all(&distances_bin).unwrap();
 
     let json_file = File::create("haversine_pairs.json").unwrap();
     let mut json_w = BufWriter::new(json_file);
-    let _ = json_w.write("{ \"pairs\": [\n".as_bytes()).unwrap();
-    for (c0, c1) in pairs[..pairs.len() - 1].iter() {
-        let _ = writeln!(
+    let _ = json_w.write(b"{ \"pairs\": [\n").unwrap();
+    for (c0, c1) in &pairs[..pairs.len() - 1] {
+        writeln!(
             json_w,
             "    {{ \"x0\": {}, \"y0\": {}, \"x1\": {}, \"y1\": {}}},",
             c0.x, c0.y, c1.x, c1.y
@@ -66,7 +65,7 @@ fn main() {
         .unwrap();
     }
     let (c0, c1) = pairs.last().expect("pairs is not empty");
-    let _ = writeln!(
+    writeln!(
         json_w,
         "    {{ \"x0\": {}, \"y0\": {}, \"x1\": {}, \"y1\": {}}}
   ]
@@ -89,25 +88,26 @@ fn generate_pairs_uniform(count: u64, mut rng: impl Rng) -> Vec<(Coordinate, Coo
 
 const CLUSTER_COUNT: u64 = 10;
 fn generate_pairs_cluster(pair_count: u64, mut rng: impl Rng) -> Vec<(Coordinate, Coordinate)> {
-    let mut pairs = Vec::with_capacity(pair_count as usize);
+    let mut pairs = Vec::with_capacity(usize::try_from(pair_count).unwrap());
     let pairs_in_cluster = pair_count / CLUSTER_COUNT;
 
-    let mut x_cluster;
-    let mut y_cluster;
-    let mut i = 0;
-    while i < pair_count {
-        let x_cluster_start = rng.random_range(X_CLUSTER_RANGE);
-        let y_cluster_start = rng.random_range(Y_CLUSTER_RANGE);
-        x_cluster = x_cluster_start..=x_cluster_start + CLUSTER_SIZE;
-        y_cluster = y_cluster_start..=y_cluster_start + CLUSTER_SIZE;
+    let x_cluster_start = rng.random_range(X_CLUSTER_RANGE);
+    let y_cluster_start = rng.random_range(Y_CLUSTER_RANGE);
+    let mut x_cluster = x_cluster_start..=x_cluster_start + CLUSTER_SIZE;
+    let mut y_cluster = y_cluster_start..=y_cluster_start + CLUSTER_SIZE;
 
-        for _ in 0..pairs_in_cluster {
-            pairs.push((
-                Coordinate::generate_pair_cluster(&mut rng, x_cluster.clone(), y_cluster.clone()),
-                Coordinate::generate_pair_cluster(&mut rng, x_cluster.clone(), y_cluster.clone()),
-            ))
+    for i in 0..pair_count {
+        if i % pairs_in_cluster == 0 {
+            let x_cluster_start = rng.random_range(X_CLUSTER_RANGE);
+            let y_cluster_start = rng.random_range(Y_CLUSTER_RANGE);
+            x_cluster = x_cluster_start..=x_cluster_start + CLUSTER_SIZE;
+            y_cluster = y_cluster_start..=y_cluster_start + CLUSTER_SIZE;
         }
-        i += pairs_in_cluster;
+
+        pairs.push((
+            Coordinate::generate_pair_cluster(&mut rng, x_cluster.clone(), y_cluster.clone()),
+            Coordinate::generate_pair_cluster(&mut rng, x_cluster.clone(), y_cluster.clone()),
+        ));
     }
     pairs
 }
@@ -118,7 +118,7 @@ struct Coordinate {
 }
 impl Coordinate {
     fn generate_pair_uniform(rng: &mut impl Rng) -> Self {
-        Coordinate {
+        Self {
             x: rng.random_range(X_RANGE),
             y: rng.random_range(Y_RANGE),
         }
@@ -129,14 +129,14 @@ impl Coordinate {
         x_cluster: RangeInclusive<f64>,
         y_cluster: RangeInclusive<f64>,
     ) -> Self {
-        Coordinate {
+        Self {
             x: rng.random_range(x_cluster),
             y: rng.random_range(y_cluster),
         }
     }
 }
 
-fn process_args() -> Options {
+fn process_args_or_bail() -> Options {
     let mut options = Options {
         method: GenerationMethod::Uniform,
         seed: 0,
@@ -146,30 +146,42 @@ fn process_args() -> Options {
     let mut args = args();
     let bin_name = args.next().expect("first argument is always binary name");
 
-    if let Some(method) = args.next() {
-        match method.as_str() {
-            "uniform" => options.method = GenerationMethod::Uniform,
-            "cluster" => options.method = GenerationMethod::Cluster,
-            _ => print_usage(&bin_name, 1),
-        }
-    }
-    if let Some(seed) = args.next() {
-        let seed = seed.parse().unwrap();
-        options.seed = seed;
-    }
-    if let Some(count) = args.next() {
-        let count = count.parse().unwrap();
-        options.count = count;
+    if let Err(message) = process_options(args, &mut options) {
+        eprintln!("Error: {message}\n");
+        {
+            let bin_name: &str = &bin_name;
+            eprintln!(
+                "Usage: {bin_name} [uniform/cluster] [random seed] [number of point pairs to generate]"
+            );
+        };
+        exit(1);
     }
 
     options
 }
 
-fn print_usage(bin_name: &str, exit_code: i32) {
-    eprintln!(
-        "Usage: {bin_name} [uniform/cluster] [random seed] [number of point pairs to generate]"
-    );
-    exit(exit_code);
+fn process_options(mut args: std::env::Args, options: &mut Options) -> Result<(), String> {
+    if let Some(method) = args.next() {
+        match method.as_str() {
+            "uniform" => options.method = GenerationMethod::Uniform,
+            "cluster" => options.method = GenerationMethod::Cluster,
+            _ => return Err(format!("Generation method can only be \"uniform\" or \"cluster\". Received \"{method}\" instead.")),
+        }
+    }
+    if let Some(seed) = args.next() {
+        let Ok(seed) = seed.parse() else {
+            return Err(format!("{seed} is not a 64 bit represantable integer"));
+        };
+        options.seed = seed;
+    }
+    if let Some(count) = args.next() {
+        let Ok(count) = count.parse() else {
+            return Err(format!("{count} is not a 64 bit represantable integer"));
+        };
+        options.count = count;
+    }
+
+    Ok(())
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -185,7 +197,7 @@ struct Options {
 }
 impl Options {
     fn print(&self) {
-        let Options {
+        let Self {
             method,
             seed,
             count,
@@ -196,6 +208,6 @@ Method: {method:?}
 Random seed: {seed}
 Pair count: {count}
             "
-        )
+        );
     }
 }
